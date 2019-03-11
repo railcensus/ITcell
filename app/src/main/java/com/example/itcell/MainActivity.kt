aa7ab7acae2951a3cbe5f1c2b51f9c2077ad7499
itcell <srailwayritcell@gm>
2019-03-11 09:35:18 +0530

    add immplemented

--- app/src/main/java/com/example/itcell/MainActivity.kt
+++ app/src/main/java/com/example/itcell/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.itcell
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calculate.setOnClickListener {
+            var n1 = num1.text.toString().toInt()
+            var n2 = num2.text.toString().toInt()
+            var sum = n1+n2
+            result.text=sum.toString()
+
+        }
+
     }
 }
